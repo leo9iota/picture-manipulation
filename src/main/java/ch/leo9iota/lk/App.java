069ea161df581feaf4ecf705ac595f4e3fd4fb20
leo9iota <nine_iota1@proton.me>
2023-01-04 21:42:19 +0100

    Initial commit

---
+++ src/main/java/ch/leo9iota/lk/App.java
@@ -0,0 +1,13 @@
+package ch.leo9iota.lk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
