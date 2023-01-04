d69f0d5c8dde453b72798deb4381b03679d61544
leo9iota <nine_iota1@proton.me>
2023-01-04 21:54:17 +0100

    Initial project setup

--- src/main/java/ch/leo9iota/lk/App.java
+++ src/main/java/ch/leo9iota/lk/App.java
@@ -1,13 +1,102 @@
 package ch.leo9iota.lk;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import javax.imageio.ImageIO;
+
 /**
- * Hello world!
- *
+ * Entry point
+ * @author Léon Kiefer
+ * @version 1.0.0 (04.01.2023)
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    public static int[][] convertToArray(String inputFileOrLink) {
+        try {
+            BufferedImage image;
+            if (inputFileOrLink.substring(0, 4).toLowerCase().equals("http")) {
+                URL imageUrl = new URL(inputFileOrLink);
+                image = ImageIO.read(imageUrl);
+                if (image == null) {
+                    System.out.println("Unable to get picutre from provided URL...");
+                }
+            } else {
+                image = ImageIO.read(new File(inputFileOrLink));
+            }
+            assert image != null;
+            int imgRows = image.getHeight();
+            int imgCols = image.getWidth();
+            int[][] pixelData = new int[imgRows][imgCols];
+            for (int i = 0; i < imgRows; i++) {
+                for (int j = 0; j < imgCols; j++) {
+                    pixelData[i][j] = image.getRGB(j, i);
+                }
+            }
+            return pixelData;
+        } catch (Exception e) {
+            System.out.println("Unable to load picture: " + e.getLocalizedMessage());
+            return null;
+        }
+    }
+    public static void convertToPicture(int[][] imgData, String fileName) {
+        try {
+            int imgRows = imgData.length;
+            int imgCols = imgData[0].length;
+            BufferedImage result = new BufferedImage(imgCols, imgRows, BufferedImage.TYPE_INT_RGB);
+            for (int i = 0; i < imgRows; i++) {
+                for (int j = 0; j < imgCols; j++) {
+                    result.setRGB(j, i, imgData[i][j]);
+                }
+            }
+            File output = new File(fileName);
+            ImageIO.write(result, "jpg", output);
+        } catch (Exception e) {
+            System.out.println("Unable to save picture: " + e.getLocalizedMessage());
+        }
+    }
+    public static int[] getRGBAFromPixel(int pixelColorValue) {
+        Color pixelColor = new Color(pixelColorValue);
+        return new int[] { pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue(), pixelColor.getAlpha() };
+    }
+    public static int getColorValueRGBA(int[] colorData) {
+        if (colorData.length == 4) {
+            Color color = new Color(colorData[0], colorData[1], colorData[2], colorData[3]);
+            return color.getRGB();
+        } else {
+            System.out.println("Incorrect number of elements in RGBA array...");
+            return -1;
+        }
+    }
+    public static void viewImageData(int[][] imageTwoD) {
+        if (imageTwoD.length > 3 && imageTwoD[0].length > 3) {
+            int[][] rawPixels = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    rawPixels[i][j] = imageTwoD[i][j];
+                }
+            }
+            System.out.println("Raw pixel data from the top left corner.");
+            System.out.print(Arrays.deepToString(rawPixels).replace("],", "],\n") + "\n");
+            int[][][] rgbPixels = new int[3][3][4];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    rgbPixels[i][j] = getRGBAFromPixel(imageTwoD[i][j]);
+                }
+            }
+            System.out.println();
+            System.out.println("Extracted RGBA pixel data from top the left corner.");
+            for (int[][] row : rgbPixels) {
+                System.out.print(Arrays.deepToString(row) + System.lineSeparator());
+            }
+        } else {
+            System.out.println("Unable to extract raw pixel data due to picture being to small...");
+        }
     }
 }
